35714a51526ba305f1c4e6f9e0f5426f606d06ee
Kelvm0ra <165766439+Kelvm0ra@users.noreply.github.com>
2026-09-14 22:51:28 +0700

    menambahkan coding filter color video

--- 1-intro.py
+++ 1-intro.py
@@ -1,21 +1,42 @@
 import cv2
 from matplotlib import pyplot as plt
 
+# Code read image
 image = cv2.imread( "image2.png")
 [h,w,c] = image.shape
 
+# Filter color image
 for i in range(h) :
     for j in range(w) :
         image [i, j, 1] = 0
         image [i, j, 0] = 0
 
-#Menampilkan Citra
+# Menampilkan Citra // Show image
 scale = 0.5
-cv2.namedWindow("Image Merah", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Image Merah", int(w * scale), int(h * scale))
-cv2.imshow("Image Merah", image)
+cv2.namedWindow("Image setelah di filter", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Image setelah di filter", int(w * scale), int(h * scale))
+cv2.imshow("Image setelah di filter", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-plt.imshow(image)
-plt.title("Image")
-plt.show()
+
+# Read Video webcam
+cap = cv2.VideoCapture(0)
+
+scale = 0.5
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame[:, :, 0] = 0
+    frame[:, :, 1] = 0
+    [h, w, c] = frame.shape
+    resized = cv2.resize(frame, (int(w * scale), int(h * scale)))
+    cv2.imshow("Webcam setelah di filter", resized)
+
+    cv2.waitKey(1)
+    if cv2.getWindowProperty("Webcam setelah di filter", cv2.WND_PROP_VISIBLE) < 1:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
